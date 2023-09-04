5c4642ad0add68e79b612cf2e51c8d526d71e745
Almuhannad <almuhannad.khalid@gmail.com>
2023-09-04 14:01:32 +0300

    added service package for later

---
+++ src/main/java/com/almuhannad/app/rest/Service/UserService.java
@@ -0,0 +1,7 @@
+package com.almuhannad.app.rest.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+}
---
+++ src/main/java/com/almuhannad/app/rest/Service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.almuhannad.app.rest.Service.impl;
+
+import com.almuhannad.app.rest.Service.UserService;
+
+public class UserServiceImpl implements UserService {
+}
